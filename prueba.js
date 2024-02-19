62ea5781893f0ceecb16215df67d4e8ffcbf354c
deivinot <davidmaso2001@gmail.com>
2024-02-19 11:10:28 +0000

    Modified

--- prueba.js
+++ prueba.js
@@ -1,24 +1,33 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+// https://stackoverflow.com/questions/40162907/w3includehtml-sometimes-includes-twice
+function xLuIncludeFile() {
+  let z, i, a, file, xhttp;
 
-const server = http.createServer((req, res) => {
-  // Manejar las solicitudes HTTP aquí
-  const filePath = path.join(__dirname, './Trabajo', req.url);
-  
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
-    } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(data);
-    }
-  });
-});
+  z = document.getElementsByTagName("*");
 
-const PORT = 3000;
+  for (i = 0; i < z.length; i++) {
+      if (z[i].getAttribute("xlu-include-file")) {
+          a = z[i].cloneNode(false);
+          file = z[i].getAttribute("xlu-include-file");
+          xhttp = new XMLHttpRequest();
 
-server.listen(PORT, () => {
-  console.log(`Servidor Node.js en ejecución en http://localhost:${8080}/`);
-});
+          xhttp.onreadystatechange = function () {
+              if (xhttp.readyState === 4 && xhttp.status === 200) {
+                  a.removeAttribute("xlu-include-file");
+                  a.innerHTML = xhttp.responseText;
+                  z[i].parentNode.replaceChild(a, z[i]);
+                  xLuIncludeFile();
+              }
+          }
+
+          // false makes the send operation synchronous, which solves a problem
+          // when using this function in short pages with Chrome. But it is
+          // deprecated on the main thread due to its impact on responsiveness.
+          // This call may end up throwing an exception someday.
+
+          xhttp.open("GET", file, false);
+          xhttp.send();
+
+          return;
+      }
+  }
+}
--- ventana.js
+++ ventana.js
@@ -1,2 +1,11 @@
 const btnAbrirModal = document.querySelector("#btn-abrir-modal");
 const btnCerrar = document.querySelector("#btn-cerrar-modal");
+const modal = document.querySelector("#modal");
+
+btnAbrirModal.addEventListener("click", ()=>{
+    modal.showModal();
+})
+
+btnCerrarModal.addEventListener("click", ()=>{
+    modal.clode();
+})
