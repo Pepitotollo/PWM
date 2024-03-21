ae28f121661d2aed2d36e5f4c356ba79307686dd
deivinot <davidmaso2001@gmail.com>
2024-03-21 10:07:38 +0000

    borrar cosas que sobraban

--- coger_datos.js
+++ coger_datos.js
@@ -1,16 +1,46 @@
 // En un entorno de navegador web
-fetch('/base_datos.json') // Realiza una solicitud HTTP para cargar el archivo JSON
-    .then(response => response.json()) // Convierte la respuesta en un objeto JavaScript
-    .then(data => {
-        // Aquí puedes trabajar con los datos del JSON
-        console.log(data);
 
-        // Por ejemplo, para acceder a la lista de productos:
-        const productos = data.productos;
-        console.log(productos);
+function rellenar_cuadrados () {
+        fetch('/base_datos.json') // Realiza una solicitud HTTP para cargar el archivo JSON
+            .then(response => response.json()) // Convierte la respuesta en un objeto JavaScript
+            .then(data => {
+                    // Aquí puedes trabajar con los datos del JSON
+                    //console.log(data);
 
-        // Para acceder a la lista de usuarios:
-        const usuarios = data.usuarios;
-        console.log(usuarios);
-    })
-    .catch(error => console.error('Error al cargar el archivo JSON:', error));
+                    // Por ejemplo, para acceder a la lista de productos:
+                    const productos = data.productos;
+                    //console.log(productos);
+
+                    // Para acceder a la lista de usuarios:
+                    const usuarios = data.usuarios;
+                    //console.log(usuarios);
+
+                    for (let i = 1; i <= 5; i++) {
+                            console.log(i);
+                    }
+            })
+            .catch(error => console.error('Error al cargar el archivo JSON:', error));
+}
+
+function cargarModuloYAsignarIDs(contenedorID, moduloID) {
+        // Obtener el contenedor donde se cargará el módulo
+        var contenedor = document.getElementById(contenedorID);
+        var elementos = contenedor.getElementsByClassName("espacio")
+
+        // Clonar el contenido del módulo
+
+        // Obtener los elementos dentro del módulo clonado
+
+        // Iterar sobre los elementos y asignar IDs únicos
+        for (var i = 0; i < elementos.length; i++) {
+
+                elementos[i].id = moduloID + "_div" + (i + 1);
+
+                console.log(elementos[i].id);
+                //contenedor.setAttribute("id", elementos[i].id);
+
+        }
+        document.getElementById(contenedorID);
+
+        // Agregar el módulo clonado al contenedor
+}
--- xlu-include-file.js
+++ xlu-include-file.js
@@ -15,6 +15,42 @@ function xLuIncludeFile() {
                   a.removeAttribute("xlu-include-file");
                   a.innerHTML = xhttp.responseText;
                   z[i].parentNode.replaceChild(a, z[i]);
+                  function xLuIncludeFile() {
+                      let z, i, a, file, xhttp;
+
+                      z = document.getElementsByTagName("*");
+
+                      for (i = 0; i < z.length; i++) {
+                          if (z[i].getAttribute("xlu-include-file")) {
+                              a = z[i].cloneNode(false);
+                              file = z[i].getAttribute("xlu-include-file");
+                              xhttp = new XMLHttpRequest();
+
+                              xhttp.onreadystatechange = function () {
+                                  if (xhttp.readyState === 4 && xhttp.status === 200) {
+                                      a.removeAttribute("xlu-include-file");
+                                      a.innerHTML = xhttp.responseText;
+                                      z[i].parentNode.replaceChild(a, z[i]);
+
+                                      //Añadido
+                                      //cargarModuloYAsignarIDs("seccion1", "espacio1");
+
+                                      xLuIncludeFile();
+                                  }
+                              }
+
+                              // false makes the send operation synchronous, which solves a problem
+                              // when using this function in short pages with Chrome. But it is
+                              // deprecated on the main thread due to its impact on responsiveness.
+                              // This call may end up throwing an exception someday.
+
+                              xhttp.open("GET", file, false);
+                              xhttp.send();
+
+                              return;
+                          }
+                      }
+                  }
                   xLuIncludeFile();
               }
           }
